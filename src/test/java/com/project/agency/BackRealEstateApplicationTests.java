5c5a20f0a41a52234ec1327ab07974e8cb9ade23
zineelabidin <bakherzineelabidine@gmail.com>
2020-01-31 12:18:35 +0100

    the first structur of the project back

---
+++ src/test/java/com/project/agency/BackRealEstateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.agency;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackRealEstateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
